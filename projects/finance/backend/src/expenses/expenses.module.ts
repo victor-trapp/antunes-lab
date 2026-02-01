333ee53c688eda5a3272aa23e771bddc5dd148a4
Victor Antunes <victorsysadm@gmail.com>
2026-02-01 16:47:44 +0000

    deleting project

--- projects/finance/backend/src/expenses/expenses.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ExpensesController } from './expenses.controller';
-import { ExpensesService } from './expenses.service';
-
-@Module({
-  controllers: [ExpensesController],
-  providers: [ExpensesService]
-})
-export class ExpensesModule {}
--- projects/finance/backend/src/income/income.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('income')
-export class IncomeController {}
