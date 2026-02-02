5e74e34078f696c54784d5eec2a0f5e13844f828
Victor Antunes <victorsysadm@gmail.com>
2026-02-02 14:44:32 +0000

    new TS folder for training

---
+++ training/GO/02-variables-1/02-types/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare
+	var daysInDecember int
+	// Assign
+	daysInDecember = 31
+
+	// Declare and assign
+	var pi float64 = 3
+	// Also declare and assign
+	learningGo := false
+
+	fmt.Println("days in december:", daysInDecember)
+	fmt.Println("pi:", pi)
+	fmt.Println("learning go:", learningGo)
+}
